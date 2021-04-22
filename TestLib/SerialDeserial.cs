ec2ff9ef1b5bc4718f269a1c7e0759ac900808fe
lozynska <olha.vdovikovska@gmail.com>
2021-04-22 13:10:25 +0300

    add TestLib

---
+++ TestLib/SerialDeserial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TestLib
+{
+   public class SerialDeserial
+    {
+        public T Deserialize<T>(String fileName) where T : class
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(T));
+            using (StreamReader sr = new StreamReader (fileName))
+            {
+                return (T)ser.Deserialize(sr);
+            }
+        }
+        public void Serialize<T>(T ObjectToSerialize, String fileName)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(ObjectToSerialize.GetType());
+            using (StreamWriter writer = new StreamWriter(fileName, false))
+            {
+                xmlSerializer.Serialize(writer, ObjectToSerialize);
+            }
+        }
+    }
+}
